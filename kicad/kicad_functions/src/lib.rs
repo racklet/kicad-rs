b2087cdc44bb99c2362a01d6957863fcf07eb7e8
Dennis Marttinen <twelho@welho.tech>
2021-06-24 14:47:20 +0300

    Add a function for indexing tuples

--- kicad/kicad_functions/src/lib.rs
+++ kicad/kicad_functions/src/lib.rs
@@ -1,3 +1,4 @@
+mod idx;
 pub mod util;
 mod vdiv;
 
@@ -6,6 +7,7 @@ use evalexpr::{EvalexprError, EvalexprResult, Value};
 // Match function for all custom functions available in the kicad_rs evaluator
 pub fn call_function(identifier: &str, argument: &Value) -> EvalexprResult<Value> {
     match identifier {
+        "idx" => idx::index(argument),
         "vdiv" => vdiv::voltage_divider(argument),
         other => Err(EvalexprError::FunctionIdentifierNotFound(other.into())),
     }
